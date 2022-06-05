e5cfa150e1eb8ec2d176f84a10bf5eb56806dc70
fermin_gicangao <fgicangao25@gmail.com>
2022-06-05 16:33:27 +0800

    Add search filter

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -23,7 +23,31 @@ class EnrollmentController extends Controller
             $limit = $request->limit;
         }
 
-        return EnrollmentResource::collection(Enrollment::paginate($limit));
+        $query = Enrollment::query();
+
+        if (!empty($request->seach)) {
+            $query->where(function ($query) use ($request) {
+                $value = $request->search;
+
+                $query->where('first_name', 'like', "%$value%")
+                    ->orWhere('last_name', 'like', "%$value%")
+                    ->orWhere('middle_name', 'like', "%$value%");
+            });
+        }
+
+        if (!empty($request->level)) {
+            $query->where('grade_level_to_enroll', $request->level);
+        }
+
+        if (!empty($request->track_id)) {
+            $query->where('track_id', $request->track_id);
+        }
+
+        if (!empty($request->strand_id)) {
+            $query->where('strand_id', $request->strand_id);
+        }
+
+        return EnrollmentResource::collection($query->paginate($limit));
     }
 
     public function show($id)
